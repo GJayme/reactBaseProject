99378782b302971533db7f4d19d14516ee66c13f
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-03-18 11:18:54 -0300

    Config eslint, prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,13 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'prettier/react'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -20,7 +22,14 @@ module.exports = {
   },
   plugins: [
     'react',
+    'prettier'
   ],
   rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'] }
+    ],
+    'import/prefer-default-export':'off'
   },
 };
